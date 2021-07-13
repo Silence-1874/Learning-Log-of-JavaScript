459457ca70faa0f281bc59e97e78783b1a2acd67
Silence <72530910+Silence-Zoe@users.noreply.github.com>
2021-07-13 22:32:59 +0800

    Create DOM.js

---
+++ DOM.js
@@ -0,0 +1,9 @@
+//DOM(Document Object Model)
+
+//获取元素的四种方法
+document.getElementById(); //范围最小，优先使用
+document.getElementsByName(); //范围次小
+document.getElementsByClassName();
+document.getElementsByTagName(); //范围最大，最后考虑使用
+
+//要在页面加载完全之后才能获取元素
